9ce8773c14f67f3f142aff5e5dd09d96ffcc70bd
Rick-Al <glycerol14@gmail.com>
2026-03-16 10:19:21 -0400

    Initial commit

---
+++ src/main/java/com/rickal/usts/USTrafficSignageClient.java
@@ -0,0 +1,10 @@
+package com.rickal.usts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class USTrafficSignageClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/rickal/usts/USTrafficSignageDataGenerator.java
@@ -0,0 +1,11 @@
+package com.rickal.usts;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class USTrafficSignageDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
